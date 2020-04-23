09708042a2af718637ffb159f05484d9e81f2005
jaemyung.kim <jaemyung.kim@navercorp.com>
2020-04-23 20:51:17 +0900

    grpc

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,20 @@
 package controllers
 
+import com.github.windbird123.helloworld.grpc.{GreeterServiceClient, HelloReply, HelloRequest}
+import com.typesafe.config.Config
 import javax.inject._
-
 import play.api.mvc._
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: AssetsFinder) extends AbstractController(cc) {
+class HomeController @Inject() (cc: ControllerComponents, greeterServiceClient: GreeterServiceClient, config: Config)(
+  implicit assetsFinder: AssetsFinder, ec: ExecutionContext
+) extends AbstractController(cc) {
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -17,8 +22,16 @@ class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: 
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index = Action {
+  def home = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def index = Action.async {
+    val request = HelloRequest("Caplin")
+    // create a gRPC request
+    val reply: Future[HelloReply] = greeterServiceClient.sayHello(request)
+    // forward the gRPC response back as a plain String on an HTTP response
+    reply.map(_.message).map(m => Ok(m))
   }
 
 }
